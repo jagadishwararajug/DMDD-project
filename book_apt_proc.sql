d5b18365299a8f646ae5ce9d21a98bd7368d260f
patelkrupali010 <patel.krupali1@northeastern.edu>
2021-12-15 19:19:04 -0500

    fixed book apt proc

--- book_apt_proc.sql
+++ book_apt_proc.sql
@@ -1,49 +1,53 @@
+SET SERVEROUT ON;
+
 create or replace PROCEDURE book_apt(email_id in VARCHAR2, apt_date in date, vac_mnf_name in VARCHAR2)
 AS
   uid NUMBER;
   count_users NUMBER;
+  cnt NUMBER;
   status varchar(10);
   prev_status VARCHAR(20);
     date_diff NUMBER;
+    vac_name varchar(20);
 BEGIN
       dbms_output.put_line('======================================');
-        dbms_output.put_line('SELECT distinct user_id into uid from users where email = '||email_id);
+--        dbms_output.put_line('SELECT distinct user_id into uid from users where email = '||email_id);
 
   SELECT distinct user_id into uid from users where email = email_id;
+--        dbms_output.put_line('============================================= USER ID ======================================' ||uid);
 
 
-      SELECT count(*) into count_users from APPOINTMENT where user_id = uid;
-        dbms_output.put_line('============================================= USER COUNT ======================================' ||count_users);
-
-    select distinct APPOINTMENT_STATUS into status
-                        from APPOINTMENT 
-                        where user_id = uid
-                        order by DATE_OF_APPOINTMENT desc
-                        FETCH FIRST 1 ROWS ONLY; 
-      dbms_output.put_line('APPOINTMENT STATUS: ' || status);
-
-        select APPOINTMENT_STATUS into prev_status from APPOINTMENT where USER_ID = uid order by appointment_id desc  fetch first 1 row only;
-      dbms_output.put_line('PREVIOUS RECORD STATUS: ' || prev_status);
-
-        SELECT ( TO_DATE(apt_date) - TO_DATE(DATE_OF_APPOINTMENT) ) INTO date_diff
-        FROM   appointment
-        where USER_ID=uid FETCH FIRST 1 ROW ONLY;
-        dbms_output.put_line('============================================= date_diff ======================================' ||date_diff);
-
-if count_users=0 then insert into APPOINTMENT(DATE_OF_APPOINTMENT,APPOINTMENT_STATUS,USER_ID,VACCINE_MNF_NAME) values(apt_date, LOWER('booked'),uid, lower(vac_mnf_name));
-      dbms_output.put_line('++++++++++++++++++++++      first appointment booked                ++++++++++++++++++');
+      SELECT count(*) into count_users from IMMUNIZATION_DETAILS where user_id = uid;
+--        dbms_output.put_line('============================================= USER COUNT ======================================' ||count_users);
+     
+     
+ if count_users=0 then insert into APPOINTMENT(DATE_OF_APPOINTMENT,APPOINTMENT_STATUS,USER_ID,VACCINE_MNF_NAME) values(apt_date, LOWER('booked'),uid, lower(vac_mnf_name));
+  dbms_output.put_line('++++++++++++++++++++++      first appointment booked                ++++++++++++++++++');
 
 
-ELSIF count_users=1 and lower(status) ='completed' and date_diff >=15
-then insert into APPOINTMENT(DATE_OF_APPOINTMENT,APPOINTMENT_STATUS,USER_ID,VACCINE_MNF_NAME) values(apt_date, LOWER('booked'),uid, lower(vac_mnf_name));
+
+
+ELSIF count_users=1
+then
+            SELECT ( TO_DATE(apt_date) - TO_DATE(DOSE_DATE) ) INTO date_diff
+            FROM   IMMUNIZATION_DETAILS
+            where USER_ID=uid;
+            
+            select distinct VACCINE_BRAND into vac_name from IMMUNIZATION_DETAILS where user_id = uid;
+
+--            dbms_output.put_line('============================================= date_diff ======================================' ||date_diff);
+            if(date_diff >= 15 and lower(vac_name)=lower(vac_mnf_name))
+            then
+            insert into APPOINTMENT(DATE_OF_APPOINTMENT,APPOINTMENT_STATUS,USER_ID,VACCINE_MNF_NAME) values(apt_date, LOWER('booked'),uid, lower(vac_mnf_name));
       dbms_output.put_line('++++++++++++++++++++++      second appointment booked                ++++++++++++++++++');
+            else
+            dbms_output.put_line('Difference between dose 1 and 2 is less than 14 days.');
+            end if;
+            
 
 ELSIF count_users>=2 
-and lower(prev_status)='failed' and date_diff >= 15
-then insert into APPOINTMENT(DATE_OF_APPOINTMENT,APPOINTMENT_STATUS,USER_ID,VACCINE_MNF_NAME) values(apt_date, LOWER('booked'),uid, lower(vac_mnf_name));
-      dbms_output.put_line('++++++++++++++++++++++      previous failed appointment booked                ++++++++++++++++++');
-
-else dbms_output.put_line('Failed to book appointment! Please try again later!');
+then 
+dbms_output.put_line('User already fully vaccinated.');
 end if;
 COMMIT;
 EXCEPTION WHEN OTHERS
@@ -55,4 +59,4 @@ END;
 
 
 
-EXEC book_apt('u.s@gmail.com', '08-FEB-21', 'pfizer');
+exec book_apt('u.s@gmail.com', '08-FEB-21', 'pfizer');
