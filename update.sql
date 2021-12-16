ff2743d311262cbfc58b659d6e2bc788cdb1f6a9
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-15 19:30:49 -0500

    Added university table update procedure

--- update.sql
+++ update.sql
@@ -83,3 +83,31 @@ end;
 
 execute UPDATE_VACCINE_CENTER_TABLE('Commonwealth', 'VACCINE_CENTER_STREET', 'Ash St');
 
+SET SERVEROUT ON;
+------------------------------------------------UNIVERSITY UPDATE-----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE UPDATE_UNIVERSITY_TABLE(uni_name in VARCHAR2, column_name in VARCHAR2, column_val in VARCHAR2)
+AS
+col_name VARCHAR2(25):=column_name;
+col_val VARCHAR2(100):=column_val;
+uniId NUMBER;
+update_query VARCHAR2(500);
+BEGIN
+  
+        select UNIVERSITY_ID into uniId FROM UNIVERSITY where UNIVERSITY_NAME=uni_name;
+        DBMS_OUTPUT.PUT_LINE(uniId);
+        update_query := 'UPDATE UNIVERSITY set '|| col_name || '=''' || col_val ||  ''' where UNIVERSITY_NAME=''' || uni_name || ''' and UNIVERSITY_ID=' || uniId; 
+        DBMS_OUTPUT.PUT_LINE(update_query);
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------------------------------------------------------------------');
+        EXECUTE IMMEDIATE  'UPDATE UNIVERSITY set '|| col_name || '=''' || col_val ||  ''' where UNIVERSITY_NAME=''' || uni_name || ''' and UNIVERSITY_ID=' || uniId; 
+COMMIT;
+EXCEPTION WHEN others then
+dbms_output.put_line('Error while inserting data into UNIVERSITY Table');
+rollback;
+dbms_output.put_line('Error: ');
+dbms_output.put_line(dbms_utility.format_error_stack);
+dbms_output.put_line('----------------------------------------------------------');
+end;
+/
+
+execute UPDATE_UNIVERSITY_TABLE('Northeastern University', 'UNIVERSITY_STREET', 'Hunting St');
+
